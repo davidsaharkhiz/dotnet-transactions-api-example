0ea2113a6f1bf9292118e5d7a27b9a9644b4995f
davidsakh <davidsakh@gmail.com>
2018-01-06 18:11:18 -0500

    tests

---
+++ TransactionAPI.Tests/Controllers/TransactionControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TransactionAPI.Controllers;
+using System.Web.Mvc;
+
+namespace TransactionAPI.Tests.Controllers
+{
+	[TestClass]
+	public class TransactionControllerTest
+	{
+		[TestMethod]
+		public void Index()
+		{
+			// Arrange
+			var controller = new TransactionController();
+
+			// Act
+			var result = controller.Index() as ViewResult;
+
+			// Assert
+			Assert.IsNotNull(result);
+		}
+
+		[TestMethod]
+		public void List()
+		{
+			// Arrange
+			var controller = new TransactionController();
+
+			// Act
+			var result = controller.List() as ViewResult;
+
+			// Assert
+			Assert.IsNotNull(result);
+		}
+
+	}
+}
