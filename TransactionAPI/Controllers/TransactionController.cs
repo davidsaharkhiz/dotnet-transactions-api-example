aebd64236c5a5cd6ef262ce7061f626c2a5b68d7
davidsakh <davidsakh@gmail.com>
2018-01-06 19:03:24 -0500

    constructor injection of api service

--- TransactionAPI/Controllers/TransactionController.cs
+++ TransactionAPI/Controllers/TransactionController.cs
@@ -4,11 +4,27 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TransactionAPI.Models;
+using TransactionAPI.Services;
 
 namespace TransactionAPI.Controllers
 {
     public class TransactionController : Controller
     {
+
+		private APIService _apiService;
+
+		/// <summary>
+		/// So, I normally wouldn't do this, but I believe we were barred from using 3PL's such as Autofac or Unity, but I still wanted to leverage constructor injection of this service
+		/// </summary>
+		public TransactionController()
+			: this(new APIService())
+		{
+		}
+
+		public TransactionController(APIService service) {
+			_apiService = service;
+		}
+
         // GET: Transaction
         public ActionResult Index()
         {
@@ -18,6 +34,9 @@ namespace TransactionAPI.Controllers
 		// GET: Transaction/Details/5
 		public ActionResult List()
 		{
+
+			
+
 			var viewModel = new ViewModels.Transaction.TransactionListViewModel
 			{
 				TransactionList = new TransactionList()
