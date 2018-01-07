123e8231fbd3a5309fe3139fa0306825911a6d6e
davidsakh <davidsakh@gmail.com>
2018-01-07 17:18:41 -0500

    Fixed bundle configuration and CSS looks a lot better now

--- TransactionAPI/App_Start/BundleConfig.cs
+++ TransactionAPI/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace TransactionAPI
 {
@@ -11,36 +10,33 @@ namespace TransactionAPI
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
 						"~/Scripts/jquery-{version}.js"));
 
+			bundles.Add(new ScriptBundle("~/bundles/sbadmin").Include(
+			"~/Scripts/bootstrap.bundle.min.js",
+			"~/Scripts/jquery.easing.min.js",
+			"~/Scripts/Chart.min.js",
+			"~/Scripts/jquery.dataTables.js",
+			"~/Scripts/dataTables.bootstrap4.js",
+			"~/Scripts/sb-admin.min.js",
+			"~/Scripts/sb-admin-datatables.min.js",
+			"~/Scripts/sb-admin-charts.min.js"));
+
 			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
 						"~/Scripts/jquery.validate*"));
 
-			// Use the development version of Modernizr to develop with and learn from. Then, when you're
-			// ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
 			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
 						"~/Scripts/modernizr-*"));
 
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-					  "~/Scripts/bootstrap.js",
-					  "~/Scripts/datatables/jquery.dataTables.js",
-					  "~/Scripts/respond.js"));
-
-			/* todo: may need more
-			 * 
-			 * 
-	<link rel="stylesheet" href="~/lib/bootstrap/dist/css/bootstrap.css" />
-	<link rel="stylesheet" href="~/css/site.css" />
-	<!-- Bootstrap core CSS-->
-	<link rel="stylesheet" href="~/vendor/bootstrap/css/bootstrap.min.css">
-	<!-- Custom fonts for this template-->
-	<link rel="stylesheet" href="~/vendor/font-awesome/css/font-awesome.min.css" type="text/css">
-*/
+				  "~/Scripts/bootstrap.js",
+				  "~/Scripts/datatables/jquery.dataTables.js",
+				  "~/Scripts/respond.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
-					  "~/Content/bootstrap.css",
-					  "~/Content/sb-admin.css",
-					  "~/Content/datatables/dataTables.bootstrap4.css",
+					  "~/Content/bootstrap.min.css",
 					  "~/Content/font-awesome/font-awesome.min.css",
-					  "~/Content/Site.css"));
+					  "~/Content/datatables/dataTables.bootstrap4.css",
+					  "~/Content/Site.css",
+					  "~/Content/sb-admin.css"));
 		}
 	}
 }
