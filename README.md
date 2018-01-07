ddeae013e1cfd41ff9ad10b0bbcb20e6e49e71f3
davidsakh <davidsakh@gmail.com>
2018-01-07 12:44:03 -0500

    manual parsing of headers

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-"Sample Transactions API" 
+Sample Transactions API
+
+This is a take-home programming exercise to demonstrate my familiarity with C#, .NET MVC, and APIs.
+
+Rules:
+
+Create a .NET solution to consume a supplied endpoint that requires a secure authorization token. Manually parse the response without the help of any third party libraries, normalize the data, and display to the user a formatted list of all transactions as well
+as a breakdown of various metrics.
--- TransactionAPI/Helpers/ParseHelper.cs
+++ TransactionAPI/Helpers/ParseHelper.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Web;
 
 namespace TransactionAPI.Helpers
 {
@@ -15,31 +14,45 @@ namespace TransactionAPI.Helpers
 		/// <summary>
 		/// Since we're not working with standard JSON here, we've defined a means to parse a generic response into something more malleable
 		/// </summary>
+		/// <throws>ArgumentException on poorly formatted response</throws>
 		/// <returns>A dictionary of keys and values representing the supplied data</returns>
 		public static Dictionary<string, string> BuildDictionaryFromTabbedAPIResponse(string rawData) {
 			var result = new Dictionary<string, string>();
 
+			var exceptionMessage = $"API Response is poorly formatted and could not be parsed by {nameof(BuildDictionaryFromTabbedAPIResponse)} ";
+
+			List<string> headersList;
 			var strReader = new StringReader(rawData);
-			string aLine;
-			string aParagraph = string.Empty;
+			string currentLine;
 
-
-
+			var headersLine = strReader.ReadLine();
+			if(headersLine != null) {
+				headersList = headersLine.Split('\t').ToList();
+			}
+			else {
+				throw new ArgumentException(exceptionMessage);
+			}
+			
 			while (true)
 			{
-				aLine = strReader.ReadLine();
-				if (aLine != null)
+				currentLine = strReader.ReadLine();
+				if (currentLine != null)
 				{
-					aParagraph = aParagraph + aLine + " ";
+					var currentItemValues = currentLine.Split('\t').ToList();
+
+					if(currentItemValues.Count() != currentLine.Count()) {
+						throw new ArgumentException($"{exceptionMessage} the details for one of the items does not match the headers.");
+					}
+
+					for(var i=0; i< headersList.Count(); i++) {
+						result.Add(headersList[i], currentItemValues[i]);
+					}
 				}
 				else
 				{
-					aParagraph = aParagraph + "\n";
 					break;
 				}
 			}
-
-
 
 			return result;
 		}
